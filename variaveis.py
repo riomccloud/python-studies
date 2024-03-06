1a649aa57db5d3c9e69da0998506eb9ee8784f01
riomccloud <riomccloud@disroot.org>
2024-03-05 22:02:40 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,31 @@
+"""
+primeiroNome: str = "Rio"
+segundoNome: str = "McCloud"
+nomeCompleto: str = primeiroNome + " " + segundoNome
+
+print(nomeCompleto)
+
+primeiroNumero: int = 532958925
+segundoNumero: int = 504398503
+soma: int = primeiroNumero + segundoNumero
+print(soma)
+
+resultado = 69 ** 256256
+print(resultado)
+
+numero1 = input("Digite um valor para operação: ")
+sinal = input("Digite o tipo de operação desejada (+ soma; - subtração; * multiplicação; / divisão; ** - potenciação):")
+  if sinal = "+"
+"""
+
+nota1: float
+nota2: float
+nota3: float
+media: float
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+print("A média calculada é " + str(media))
